0b84fee2bffeece7b044bc4809105f633b0b5a17
Vatan Soni <vatansoni89@gmail.com>
2020-10-08 16:05:07 +0530

    Calculator with Add operation

--- HelloWpf/MainWindow.xaml.cs
+++ HelloWpf/MainWindow.xaml.cs
@@ -20,9 +20,90 @@ namespace HelloWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        SelectedOperator selectedOperator;
+        double lastNumber;
         public MainWindow()
         {
             InitializeComponent();
+            resultLabel.Content = "0";
+            btnMultNegative.Click += BtnMultNegative_Click;
+            btnAC.Click += BtnAC_Click;
         }
+
+        private void BtnAC_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+
+        private void BtnMultNegative_Click(object sender, RoutedEventArgs e)
+        {
+            double.TryParse(resultLabel.Content.ToString(), out double num);
+            resultLabel.Content = (num * (-1)).ToString();
+        }
+
+        private void BtnNumber_Click(object sender, RoutedEventArgs e)
+        {
+            var number = sender as Button;
+
+            var num = number.Content.ToString();
+            if (resultLabel.Content.ToString() != "0")
+            {
+                resultLabel.Content = $"{resultLabel.Content}{num}";
+            }
+            else
+            {
+                resultLabel.Content = num;
+            }
+        }
+
+        private void OperationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            double.TryParse(resultLabel.Content.ToString(), out lastNumber);
+
+            resultLabel.Content = 0;
+
+            if (sender == BtnAdd)
+            {
+                selectedOperator = SelectedOperator.Add;
+            }
+        }
+
+        private void BtnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out double newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Add:
+                        resultLabel.Content = SimpleMath.Add(newNumber, lastNumber);
+                        break;
+                    case SelectedOperator.Subsn:
+                        break;
+                    case SelectedOperator.Mult:
+                        break;
+                    case SelectedOperator.Divide:
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+    }
+
+    public class SimpleMath
+    { 
+        public static double Add(double n1, double n2)
+        { 
+            return n1+n2;
+        }
+    }
+
+    public enum SelectedOperator
+    {
+        Add,
+        Subsn,
+        Mult,
+        Divide
     }
 }
