5ff27b9a9863180bc9ca1b45aad3eedfe6eb589f
Vatan Soni <vatansoni89@gmail.com>
2020-10-14 16:36:39 +0530

    Custom Control & Binding CU-Dependency

---
+++ DesktopContactsApp/Controlls/ContactControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using DesktopContactsApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp.Controlls
+{
+    /// <summary>
+    /// Interaction logic for ContactControl.xaml
+    /// </summary>
+    public partial class ContactControl : UserControl
+    {
+        //propdp for creating dependency prop
+
+        public Contact Contact
+        {
+            get { return (Contact)GetValue(ContactProperty); }
+            set { SetValue(ContactProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Contact.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactProperty =
+            DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactControl), new PropertyMetadata(new Contact(){ Name = "",Email="", Phone=""}, SetText));
+
+        private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ContactControl control = d as ContactControl;
+            if (control!=null)
+            {
+                control.nameTextBox.Text = (e.NewValue as Contact).Name;
+                control.emailTextBox.Text = (e.NewValue as Contact).Email;
+                control.phoneTextBox.Text = (e.NewValue as Contact).Phone;
+            }
+        }
+
+        public ContactControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
